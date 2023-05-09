55d14e43617edc6365d06da16d2acad6e4b55ab6
seymoreg <seymur.gul1y3v@gmail.com>
2023-05-10 01:44:12 +0400

    skeleton

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#00704A", // green
+      secondary: "#27251F", // black
+    },
+  },
+  plugins: [],
+};
